6fef1e81fb1098a364ce794699490f063258eb9c
HassanKun <ev292il@gmail.com>
2023-02-16 22:28:10 +0300

    Solve 01 and 02

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,10 @@
-const repeatString = function() {
-
+const repeatString = function(mystring,repatednum) {
+    if (repatednum < 0) return 'ERROR';
+    let repeatedstring = '';
+    for (let i = 0; i <repatednum; i++){
+        repeatedstring+=mystring;
+    }
+    return repeatedstring;
 };
 
 // Do not edit below this line
